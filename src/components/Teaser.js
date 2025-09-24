14a4c0ae93d827d084dc07b6b4a4f29604d1168c
Mario Flores <floresperezmarioandres@gmail.com>
2025-09-23 19:02:36 -0600

    ignorara

--- src/components/Teaser.js
+++ src/components/Teaser.js
@@ -4,8 +4,9 @@ const Teaser = ({ blok }) => {
   return (
     <h2 style={{ textAlign: "center" }} {...storyblokEditable(blok)}>
       {blok.headline}
-      <br>m</br>
+      {' Ignorar'}
     </h2>
+    
   );
 };
 
